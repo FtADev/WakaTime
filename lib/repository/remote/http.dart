0ba790cb4c55ec0f479a38c0dcff5d9cfa377fde
ftadev <akhlaghi.fatemeh@gmail.com>
2020-02-15 13:49:46 +0330

    - Unused url

--- lib/repository/remote/http.dart
+++ lib/repository/remote/http.dart
@@ -1,9 +1,5 @@
-import 'dart:convert';
-
 import 'package:dio/dio.dart';
-import 'package:waka/repository/model/user.dart';
 import 'package:waka/repository/model/user_data.dart';
-import 'package:waka/repository/model/users_activity.dart';
 import 'package:waka/repository/remote/url.dart';
 
 BaseOptions options = BaseOptions(
@@ -14,84 +10,6 @@ BaseOptions options = BaseOptions(
 
 Dio dio = Dio(options);
 
-Future<User> login(String username, String password) async {
-  Response response;
-  var url = URLs.LOGIN;
-  print(url);
-  try {
-    response = await dio.post(url,
-        data: json.encode({"username": username, "password": password}));
-    if (response.statusCode == 200) {
-      print(response.data.toString());
-      return User.fromJson(response.data);
-    } else {
-      return null;
-    }
-  } on DioError catch (e) {
-    if (e.response != null) {
-      print(e.response.data);
-      print(e.response.headers);
-      print(e.response.request);
-    } else {
-      print(e.request.path);
-      print(e.message);
-    }
-  }
-  return null;
-}
-
-Future<User> register(String apiKey, String username, String password) async {
-  Response response;
-  var url = URLs.REGISTER;
-  print(url);
-  try {
-    response = await dio.post(url,
-        data: json.encode(
-            {"api_key": apiKey, "username": username, "password": password}));
-    if (response.statusCode == 200) {
-      print(response.data.toString());
-      return User.fromJson(response.data);
-    } else {
-      return null;
-    }
-  } on DioError catch (e) {
-    if (e.response != null) {
-      print(e.response.data);
-      print(e.response.headers);
-      print(e.response.request);
-    } else {
-      print(e.request.path);
-      print(e.message);
-    }
-  }
-  return null;
-}
-
-Future<UsersActivity> getAllUsersSummary(String start, String end) async {
-  Response response;
-  var url = URLs.ALL_USER_SUMMARY + start + "/" + end;
-  print(url);
-  try {
-    response = await dio.get(url);
-    if (response.statusCode == 200) {
-      print(response.data.toString());
-      return UsersActivity.fromJson(response.data);
-    } else {
-      return null;
-    }
-  } on DioError catch (e) {
-    if (e.response != null) {
-      print(e.response.data);
-      print(e.response.headers);
-      print(e.response.request);
-    } else {
-      print(e.request.path);
-      print(e.message);
-    }
-  }
-  return null;
-}
-
 Future<UserData> getUserSummary(String apiKey, String start, String end) async {
   Response response;
   var url = URLs.GET_USER_SUMMARY + "?start=$start&end=$end&api_key=$apiKey";
@@ -101,31 +19,6 @@ Future<UserData> getUserSummary(String apiKey, String start, String end) async {
     if (response.statusCode == 200) {
       print(response.data.toString());
       return UserData.fromJson(response.data);
-    } else {
-      return null;
-    }
-  } on DioError catch (e) {
-    if (e.response != null) {
-      print(e.response.data);
-      print(e.response.headers);
-      print(e.response.request);
-    } else {
-      print(e.request.path);
-      print(e.message);
-    }
-  }
-  return null;
-}
-
-Future<UsersActivity> getAllUsers97Summary(String start, String end) async {
-  Response response;
-  var url = URLs.ALL_USER97_SUMMARY + start + "/" + end;
-  print(url);
-  try {
-    response = await dio.get(url);
-    if (response.statusCode == 200) {
-      print(response.data.toString());
-      return UsersActivity.fromJson(response.data);
     } else {
       return null;
     }
--- lib/repository/remote/url.dart
+++ lib/repository/remote/url.dart
@@ -1,8 +1,3 @@
 class URLs {
-  static const String BASE_URL = "http://151.80.200.124:3003/";
-  static const String LOGIN = "users/login";
-  static const String REGISTER = "users/register";
-  static const String ALL_USER_SUMMARY = "users/getAllUser/"; /* /:userId/:start/:end */
-  static const String ALL_USER97_SUMMARY = "users/getAllUser97/"; /* /:userId/:start/:end */
-  static const String GET_USER_SUMMARY = "https://wakatime.com/api/v1/users/current/summaries"; /* /:userId/:start/:end */
+  static const String GET_USER_SUMMARY = "https://wakatime.com/api/v1/users/current/summaries";
 }
