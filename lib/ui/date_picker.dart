8e59323e4086f9e9161dd59fbb0f0dc0086009f3
ftadev <akhlaghi.fatemeh@gmail.com>
2020-02-16 15:03:07 +0330

    Separate date picker file

---
+++ lib/ui/date_picker.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'date_time_field.dart';
+
+class DateTimeForm extends StatefulWidget {
+  @override
+  _DateTimeFormState createState() => _DateTimeFormState();
+}
+
+class _DateTimeFormState extends State<DateTimeForm> {
+  final formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          BasicDateField(),
+
+        ],
+      ),
+    );
+  }
+}
+
+class BasicDateField extends StatelessWidget {
+  final format = DateFormat("yyyy-MM-dd");
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      Text('Choose Start Day:'),
+      DateTimeField(
+        format: format,
+        onShowPicker: (context, currentValue) {
+          return showDatePicker(
+              context: context,
+              firstDate: DateTime(1900),
+              initialDate: currentValue ?? DateTime.now(),
+              lastDate: DateTime(2100));
+        },
+      ),
+      Text('Choose End Day:'),
+      DateTimeField(
+        format: format,
+        onShowPicker: (context, currentValue) {
+          return showDatePicker(
+              context: context,
+              firstDate: DateTime(1900),
+              initialDate: currentValue ?? DateTime.now(),
+              lastDate: DateTime(2100));
+        },
+      ),
+    ]);
+  }
+}
--- lib/ui/screens/user_activity_screen.dart
+++ lib/ui/screens/user_activity_screen.dart
@@ -6,7 +6,6 @@ import 'package:waka/repository/model/data.dart';
 import 'package:waka/repository/model/user_data.dart';
 import 'package:waka/repository/remote/http.dart';
 import 'package:waka/ui/activity_chart.dart';
-import 'package:waka/ui/date_time_field.dart';
 import 'package:waka/ui/editor_chart.dart';
 import 'package:waka/ui/language_chart.dart';
 import 'package:waka/ui/os_chart.dart';
@@ -109,59 +108,5 @@ class _UserActivityScreenState extends State<UserActivityScreen> {
         ),
       ),
     );
-  }
-}
-
-
-class DateTimeForm extends StatefulWidget {
-  @override
-  _DateTimeFormState createState() => _DateTimeFormState();
-}
-
-class _DateTimeFormState extends State<DateTimeForm> {
-  final formKey = GlobalKey<FormState>();
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          BasicDateField(),
-
-        ],
-      ),
-    );
-  }
-}
-
-class BasicDateField extends StatelessWidget {
-  final format = DateFormat("yyyy-MM-dd");
-  @override
-  Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      Text('Choose Start Day:'),
-      DateTimeField(
-        format: format,
-        onShowPicker: (context, currentValue) {
-          return showDatePicker(
-              context: context,
-              firstDate: DateTime(1900),
-              initialDate: currentValue ?? DateTime.now(),
-              lastDate: DateTime(2100));
-        },
-      ),
-      Text('Choose End Day:'),
-      DateTimeField(
-        format: format,
-        onShowPicker: (context, currentValue) {
-          return showDatePicker(
-              context: context,
-              firstDate: DateTime(1900),
-              initialDate: currentValue ?? DateTime.now(),
-              lastDate: DateTime(2100));
-        },
-      ),
-    ]);
   }
 }
